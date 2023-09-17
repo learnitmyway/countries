a07273d010865ffa9035fc9931b51110e1f4e033
David <learnitmyway@users.noreply.github.com>
2023-09-17 13:49:29 +0200

    in-source test

--- src/countries/Countries/CountryCard.util.ts
+++ src/countries/Countries/CountryCard.util.ts
@@ -1,3 +1,12 @@
+import { expect, test } from "vitest";
+
 export function joinCapitals({ capitals }: { capitals?: string[] }) {
   return capitals?.join(", ") ?? "";
+}
+
+if (import.meta.vitest) {
+  test("joins capitals", () => {
+    expect(joinCapitals({ capitals: undefined })).toBe("");
+    expect(joinCapitals({ capitals: ["foo", "bar"] })).toBe("foo, bar");
+  });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" />
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  test: {
+    includeSource: ["src/**/*.{js,ts}"],
+  },
   plugins: [react()],
   resolve: {
     alias: {
