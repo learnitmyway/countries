fedc1107a29cfdd5698194bd9e39803541a3add9
David <learnitmyway@users.noreply.github.com>
2023-09-22 20:46:44 +0200

    change folder structure of shadcn

--- frontend/src/countries/Countries/CountryCard.tsx
+++ frontend/src/countries/Countries/CountryCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/shadcn/ui/card";
 import { Country } from "../types";
 import { joinList } from "./util";
 
--- frontend/src/shadcn/ui/card.tsx
+++ frontend/src/shadcn/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/shadcn/utils";
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
