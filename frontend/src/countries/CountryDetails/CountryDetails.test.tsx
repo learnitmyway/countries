f902a12b6adf6148498b1f02dea13d45c2ab84ce
David <learnitmyway@users.noreply.github.com>
2023-09-25 23:04:37 +0200

    FE: remove unused import

--- frontend/src/countries/CountryDetails/CountryDetails.test.tsx
+++ frontend/src/countries/CountryDetails/CountryDetails.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { CountryDetails } from "./CountryDetails";
 import { createQueryClient, customRender } from "../../test/customRender";
 import { screen, waitForElementToBeRemoved } from "@testing-library/react";
