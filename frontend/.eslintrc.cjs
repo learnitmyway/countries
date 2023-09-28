42bb0fb6b17d42d3d7ac420b26ea68794a93cefe
David <learnitmyway@users.noreply.github.com>
2023-09-28 12:15:06 +0200

    add vitest lint plugin

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,11 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:react/jsx-runtime",
-    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "plugin:vitest/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs", "prettier.config.cjs"],
   parser: "@typescript-eslint/parser",
@@ -17,7 +18,7 @@ module.exports = {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
-  plugins: ["react-refresh", "@tanstack/query"],
+  plugins: ["react-refresh", "@tanstack/query", "vitest"],
   rules: {
     "no-console": "warn",
     "react-refresh/only-export-components": [
