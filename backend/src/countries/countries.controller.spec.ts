3cd8ea20280a2696799e079f0167fd96d20107d7
David <learnitmyway@users.noreply.github.com>
2023-10-07 21:59:55 +0200

    BE: fix broken test

--- backend/src/countries/countries.controller.spec.ts
+++ backend/src/countries/countries.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CountriesController } from './countries.controller';
 import { PrismaService } from '../prisma.service';
+import { CountriesService } from './countries.service';
 
 describe('CountriesController', () => {
   let controller: CountriesController;
@@ -8,7 +9,7 @@ describe('CountriesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CountriesController],
-      providers: [PrismaService],
+      providers: [PrismaService, CountriesService],
     }).compile();
 
     controller = module.get<CountriesController>(CountriesController);
